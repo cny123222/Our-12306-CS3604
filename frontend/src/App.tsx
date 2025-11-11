d70031f67349c3e17e7f85f42efa2056fe0ae106
cny123222 <cny123222@sjtu.edu.cn>
2025-11-11 09:17:17 +0800

    Finish basic register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/" element={<LoginPage />} />
       </Routes>
     </div>
